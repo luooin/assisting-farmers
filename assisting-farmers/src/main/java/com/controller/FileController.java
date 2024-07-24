c5caeff79222f07c65bbd86b9423d79815d7c888
luocong <2369303335@qq.com>
2024-07-24 14:50:25 +0800

    最终版

---
+++ assisting-farmers/src/main/java/com/controller/FileController.java
@@ -0,0 +1,113 @@
+package com.controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.annotation.IgnoreAuth;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.entity.ConfigEntity;
+import com.entity.EIException;
+import com.service.ConfigService;
+import com.utils.R;
+
+/**
+ * 上传文件映射表
+ */
+@RestController
+@RequestMapping("file")
+@SuppressWarnings({"unchecked","rawtypes"})
+public class FileController{
+	@Autowired
+    private ConfigService configService;
+	/**
+	 * 上传文件
+	 */
+	@RequestMapping("/upload")
+	public R upload(@RequestParam("file") MultipartFile file,String type) throws Exception {
+		if (file.isEmpty()) {
+			throw new EIException("上传文件不能为空");
+		}
+		String fileExt = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+		File path = new File(URLDecoder.decode(ResourceUtils.getURL("classpath:static").getPath(), StandardCharsets.UTF_8.toString()));// 这里拿到的是target的路径，例如D:/project/target/resources/static，反正就是字节流文件的路径，不是src里面的
+
+		File upload = new File(path.getAbsolutePath(),"/upload/");
+		if(!upload.exists()) {
+		    upload.mkdirs();
+		}
+		String fileName = new Date().getTime()+"."+fileExt;
+		File dest = new File(upload.getAbsolutePath()+"/"+fileName);
+		file.transferTo(dest);
+
+		FileUtils.copyFile(dest, new File("src/main/resources/static/upload/"+fileName)); /**因为target里面的是运行时数据，当下次启动的时候就会消失，因此需要将图片数据持久化保存到同类目录下，即src/main/resources/static/upload/，这样当下次项目启动的时候，target里面就会有原来的数据，而前端就可以通过http:8080/upload/17xxxx1.png直接访问到target里面的图片数据**/
+
+		if(StringUtils.isNotBlank(type) && type.equals("1")) {
+			ConfigEntity configEntity = configService.selectOne(new EntityWrapper<ConfigEntity>().eq("name", "faceFile"));
+			if(configEntity==null) {
+				configEntity = new ConfigEntity();
+				configEntity.setName("faceFile");
+				configEntity.setValue(fileName);
+			} else {
+				configEntity.setValue(fileName);
+			}
+			configService.insertOrUpdate(configEntity);
+		}
+		return R.ok().put("file", fileName);
+	}
+	
+	/**
+	 * 下载文件
+	 */
+	@IgnoreAuth
+	@RequestMapping("/download")
+	public ResponseEntity<byte[]> download(@RequestParam String fileName) {
+		try {
+			File path = new File(ResourceUtils.getURL("classpath:static").getPath());
+			if(!path.exists()) {
+			    path = new File("");
+			}
+			File upload = new File(path.getAbsolutePath(),"/upload/");
+			if(!upload.exists()) {
+			    upload.mkdirs();
+			}
+			File file = new File(upload.getAbsolutePath()+"/"+fileName);
+			if(file.exists()){
+				/*if(!fileService.canRead(file, SessionManager.getSessionUser())){
+					getResponse().sendError(403);
+				}*/
+				HttpHeaders headers = new HttpHeaders();
+			    headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);    
+			    headers.setContentDispositionFormData("attachment", fileName);    
+			    return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),headers, HttpStatus.CREATED);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<byte[]>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+}
